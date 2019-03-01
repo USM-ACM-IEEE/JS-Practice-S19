59b35372b00e40cf79ad3218d759fc4088be6855
wlkeith <whitneylynnkeith@gmail.com>
2019-02-28 20:46:19 -0600

    Whitney's solved practice problems

---
+++ WhitneyK/problem3.js
@@ -0,0 +1,12 @@
+
+
+let antarctica = [{"country_name":"Antarctica","country_abbreviation":"AQ","capital":null},{"country_name":"Bouvet Island","country_abbreviation":"BV","capital":null},{"country_name":"French Southern territories","country_abbreviation":"TF","capital":null},{"country_name":"Heard Island and McDonald Islands","country_abbreviation":"HM","capital":null},{"country_name":"South Georgia and the South Sandwich Islands","country_abbreviation":"GS","capital":null}];
+
+
+for (const prop in antarctica) {
+    
+    console.log(antarctica[prop].country_name);
+    console.log(antarctica[prop].country_abbreviation);
+    
+}
+
